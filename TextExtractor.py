57502855a7262765bab280690def862147b8791f
Asaf <apzodon@gmail.com>
2019-03-29 05:43:46 +0300

    sentence comparing

--- TextExtractor.py
+++ TextExtractor.py
@@ -1,3 +1,8 @@
+import json
+import os
+
+from sentence_comparing import SentenceComparator
+from universal_sentence_encoder import UniversalSentenceEncoder
 from utils import Worker
 
 
@@ -5,6 +10,15 @@ class TextExtractor(Worker):
     def _step(self):
         val = self.queue.get()
         self.action(val)
+        self.sentence_comparator = self._get_sentence_comparator()
 
     def action(self, message):
         pass
+
+    @staticmethod
+    def _get_sentence_comparator():
+        module_url = "https://tfhub.dev/google/universal-sentence-encoder-large/3"
+        encoder = UniversalSentenceEncoder(module_url)
+        sentences_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sentences.json")
+        sentences = json.load(sentences_file)
+        return SentenceComparator(encoder, sentences)
--- sentence_comparing.py
+++ sentence_comparing.py
@@ -1,8 +1,17 @@
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 class SentenceComparator:
-    def __init__(self, sentences, encoder):
+    def __init__(self, sentences, encoder, threshold=0.5):
         self.encoder = encoder
         self.sentence_vectors_list = encoder.encode(sentences)
         self.sentences = sentences
+        self.threshold = threshold
 
-    def get_most_similar(self, sentence):
-        sentence_vec = self.encoder.encode(sentence)
+    def get_most_similar(self, input_sentence):
+        input_sentence_vec = self.encoder.encode(input_sentence).reshape(-1)
+        distances = cosine_similarity(input_sentence_vec, self.sentence_vectors_list)
+        max_index = np.argmax(distances)
+        max_value = distances[max_index]
+        if max_value < self.threshold:
+            return None
+        return self.sentences[max_index]
